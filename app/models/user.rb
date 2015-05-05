c0abcc0a5a17786260599089129206d78170d003
Spencer Olson <spence1818@gmail.com>
2015-05-04 21:36:07 -0500

    change user to use  has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
-	include BCrypt
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
 
-	has_many :subscriptions
-	has_many :groups, through: :subscriptions, as: :member
-	has_many :created_groups, foreign_key: "creator_id", class_name: "Group", dependent: :destroy
+  has_many :subscriptions
+  has_many :groups, through: :subscriptions, as: :member
+  has_many :created_groups, foreign_key: "creator_id", class_name: "Group", dependent: :destroy
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  validates :name, :email, presence: true
+  validates :password, length: { minimum: 8 }
 end
--- db/migrate/20150501042004_create_groups.rb
+++ db/migrate/20150501042004_create_groups.rb
@@ -1,13 +1,13 @@
 class CreateGroups < ActiveRecord::Migration
   def up
     create_table :groups do |t|
-    	t.string :name, null: false
-    	t.references :creator, index: true
+      t.string :name, null: false
+      t.references :creator, index: true
       t.timestamps null: false
     end
   end
 
   def down
-  	drop_table :groups
+    drop_table :groups
   end
 end
